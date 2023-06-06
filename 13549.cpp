55a909161de36403fd0e59132c57613f6ae855d8
hyungyu <yona20020209@gmail.com>
2023-06-06 14:32:48 +0000

    13549

--- 13549.cpp
+++ 13549.cpp
@@ -1,33 +1,24 @@
 #include <iostream>
-#include <set>
+#include <queue>
 
 using namespace std;
 
 int n, k;
-set<pair<int,int>> s; //time, pos
+int weight[100000] = {100000, };
+queue<int> q;
+
+void findWeight(int pos){
+	if(pos == k){
+		cout<<
+	}
+}
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	
 	cin>>n>>k;
 	
-	s.insert({0,n});
-	
-	pair<int,int> p;
-	int pos, time;
-	while(!q.empty()){
-		p = *(s.begin());
-		time = p.first;
-		pos = p.second;
-		s.erase(s.begin());
-		
-		if(pos == k){
-			cout<<time;
-			return 0;
-		}
-		
-		s.insert({time,pos*2})
-	}
-	
+	findWeight(n);
 	
 	
 	return 0;
